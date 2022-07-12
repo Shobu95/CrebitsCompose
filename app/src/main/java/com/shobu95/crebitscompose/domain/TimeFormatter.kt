6c172660bf78c48c62d0a3656fed9aeed948d660
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-07-12 14:58:18 +0500

    Add Transaction screen created

---
+++ app/src/main/java/com/shobu95/crebitscompose/domain/TimeFormatter.kt
@@ -0,0 +1,20 @@
+package com.shobu95.crebitscompose.domain
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun getCurrentHourMinute(): Pair<Int, Int> {
+    val mCalendar = Calendar.getInstance()
+    val mHour = mCalendar[Calendar.HOUR_OF_DAY]
+    val mMinute = mCalendar[Calendar.MINUTE]
+    return Pair(mHour, mMinute)
+}
+
+
+fun convertTo12Hour(_24HourTime: String): String {
+    val _24HourSDF = SimpleDateFormat("HH:mm")
+    val _12HourSDF = SimpleDateFormat("hh:mm a")
+    val _24HourDt = _24HourSDF.parse(_24HourTime)
+    return _12HourSDF.format(_24HourDt)
+}
