666fe9fcfe0c80f5b504818b31d12c9011ba3f74
Shoaib.Ahmed <shoaib.ahmed@avanzasolutions.com>
2022-07-19 10:33:19 +0500

    Bold style removed from amount text in TransactionItem.kt

--- app/src/main/java/com/shobu95/crebitscompose/ui/screens/transactions/list/components/TransactionItem.kt
+++ app/src/main/java/com/shobu95/crebitscompose/ui/screens/transactions/list/components/TransactionItem.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,7 +66,6 @@ fun TransactionItem(
                 Text(
                     text = "Rs.${transaction.amount}",
                     fontSize = 22.sp,
-                    fontWeight = FontWeight.Bold
                 )
 
                 Spacer(modifier = Modifier.height(4.dp))
